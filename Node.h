f27a736d94ca1e20dde4655a5e60675fa6f3267e
FathimaHamna <fathimahamna497@gmail.com>
2022-11-30 18:50:18 +0530

    v2

--- Node.h
+++ Node.h
@@ -5,6 +5,6 @@ using namespace std;
 class Node{
 public:
     int value;
-    Node* next; //pointer to the next node
-    Node* prev; //pointer to the previous node
+    Node* next;  //pointer to the next node
+    Node* prev;  //pointer to the previous node
 }
